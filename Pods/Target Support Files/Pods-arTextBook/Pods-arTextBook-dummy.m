48ce2ad47010e5a3f7b73c2ed882d81ec9ff9c96
james luo <luojames52@gmail.com>
2019-09-06 22:15:21 -0400

    first push

---
+++ Pods/Target Support Files/Pods-arTextBook/Pods-arTextBook-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_arTextBook : NSObject
+@end
+@implementation PodsDummy_Pods_arTextBook
+@end
--- arTextBook/ViewController.swift
+++ arTextBook/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
-
+import Firebase
+import FirebaseDatabase
+import SwiftyJSON
 class ViewController: UIViewController {
-
+    var myClassCode = "123"
+    var myAry = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var EnteredCode: UITextField!
+    
+    
+   
+    func textFieldShouldReturn(_ EnteredCode: UITextField) -> Bool {
+        myClassCode = EnteredCode.text ?? ""
+        print()
+        self.view.endEditing(true)
+        return false
+    }
+    
+    @IBAction func LoadDataBase(_ sender: UIButton) {
+        let ref = Database.database().reference()
+        let myData = ref.child(myClassCode)
+        myData.observeSingleEvent(of: .value, with: {
+            (snapshot) in
+            let myValue = JSON(snapshot.value as Any)
+            let myDataAry = myValue.arrayValue
+            for myData in myDataAry{
+                let myBook = myData["Name"].string
+                if (myBook != nil){
+                    self.myAry.append(myBook!)
+                }
+                
+            }
+            print(self.myAry)
+            
+        })
+    }
 }
 
