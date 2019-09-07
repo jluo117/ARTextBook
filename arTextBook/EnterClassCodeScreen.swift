5271ac8f19243158f122ad339cfc390819089f73
Iccha Singh <icchasingh@huntsman-ve702-6876.apng.wlan.private.upenn.edu>
2019-09-07 00:49:01 -0400

    wow

--- arTextBook/EnterClassCodeScreen.swift
+++ arTextBook/EnterClassCodeScreen.swift
@@ -23,22 +23,34 @@ class EnterClassCodeScreen: UIViewController {
         
         setupViewController()
         setUpLayout()
-        
+        loadTextbooks()
     }
     
     lazy var entryLabel: UITextField = {
-        let label = UITextField(frame: CGRect(x: 30, y: 60, width: self.view.frame.width * 0.8, height: 60))
+        let label = UITextField(frame: .zero)
         label.text = "Hello World"
         
         return label
     }()
     
     lazy var entryButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 30, y: 40, width: self.view.frame.width * 0.8, height: 30))
-        button.titleLabel?.text = "Log in"
+        let button = UIButton(frame: .zero)
+        
+        button.backgroundColor = UIColor.black
+        button.titleLabel?.textColor = UIColor.white
+        button.setTitle("Log in", for: .normal)
+        button.addTarget(self, action: #selector(loadTextbooks), for: .touchDown) //button click tiggers calling of function
         
         return button
     }()
+    
+    
+    @objc func loadTextbooks() { //objective C function
+        
+        print("loading!")
+        
+    }
+    
     
     func setupViewController() {
         
@@ -51,19 +63,24 @@ class EnterClassCodeScreen: UIViewController {
     func setUpLayout() {
         
         entryField.translatesAutoresizingMaskIntoConstraints = false
+        entryLabel.translatesAutoresizingMaskIntoConstraints = false
+        entryButton.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             entryField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0), //offset from the center
             entryField.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -200),
             entryField.widthAnchor.constraint(equalToConstant: self.view.frame.width * 0.8),
             entryField.heightAnchor.constraint(equalToConstant: self.view.frame.height * 0.05),
             
+            entryLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
+            entryLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -270),
+            
+            entryButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
+            entryButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 100),
+            entryButton.widthAnchor.constraint(equalToConstant: self.view.frame.width * 0.5),
+            entryButton.heightAnchor.constraint(equalToConstant: self.view.frame.height * 0.05)
             ])
         
-        entryLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            entryLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
-            entryLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -270)
-            ])
     }
     
 }
