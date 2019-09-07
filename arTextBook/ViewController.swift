00282d5ccbc966f43a485b7e88d9534b7c6cd474
james luo <luojames52@gmail.com>
2019-09-06 22:58:32 -0400

    log in working

--- arTextBook/ViewController.swift
+++ arTextBook/ViewController.swift
@@ -10,31 +10,45 @@ import UIKit
 import Firebase
 import FirebaseDatabase
 import SwiftyJSON
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     var myClassCode = "123"
     var myAry = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.EnteredCode.delegate = self
         // Do any additional setup after loading the view.
     }
     @IBOutlet weak var EnteredCode: UITextField!
     
     
    
-    func textFieldShouldReturn(_ EnteredCode: UITextField) -> Bool {
+    
+    @IBAction func TextAction(_ sender: UITextField) {
+        EnteredCode.becomeFirstResponder()
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        EnteredCode.resignFirstResponder()
         myClassCode = EnteredCode.text ?? ""
-        print()
-        self.view.endEditing(true)
-        return false
+        return true
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        EnteredCode.text = ""
     }
     
     @IBAction func LoadDataBase(_ sender: UIButton) {
         let ref = Database.database().reference()
         let myData = ref.child(myClassCode)
+        self.myAry = []
         myData.observeSingleEvent(of: .value, with: {
             (snapshot) in
             let myValue = JSON(snapshot.value as Any)
+            if myValue == nil{
+                let alertController = UIAlertController(title: "invalid Class ID", message:
+                    "Enter a Valid Class Code", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+                
+                self.present(alertController, animated: true, completion: nil)
+            }
             let myDataAry = myValue.arrayValue
             for myData in myDataAry{
                 let myBook = myData["Name"].string
@@ -43,6 +57,7 @@ class ViewController: UIViewController {
                 }
                 
             }
+            
             print(self.myAry)
             
         })
