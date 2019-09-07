d8942642b43502b8b8e8503afc0ada493027f8db
james luo <luojames52@gmail.com>
2019-09-06 20:28:17 -0400

    Initial Commit

---
+++ arTextBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  arTextBook
+//
+//  Created by james luo on 9/6/19.
+//  Copyright © 2019 james luo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
